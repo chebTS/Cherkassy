f6c68da59cb944a2aa0c09b9de416cf1abd70608
chebTS <chebTS@gmail.com>
2013-03-26 01:44:23 +0200

    maps added

--- src/org/geekhub/cherkassy/fragments/MapFragment.java
+++ src/org/geekhub/cherkassy/fragments/MapFragment.java
@@ -6,14 +6,64 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
-public class MapFragment extends SherlockFragment {
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MapFragment extends SherlockFragment  {
+	private MapView mMapView;
+    private GoogleMap mMap;
+    private Bundle mBundle;
+	
+	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.map_frag, container,false);
+		if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(getSherlockActivity()) == 0){				
+			mMapView = (MapView) v.findViewById(R.id.map);
+	        mMapView.onCreate(mBundle);
+	        mMap = ((MapView) v.findViewById(R.id.map)).getMap();
+	        mMap.setMyLocationEnabled(true);
+	        //mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(37.412259, -79.138257) , 13.0f) );
+	        //mMap.setOnInfoWindowClickListener(this);
+	        //addMarkers();
+	        
+		}
         return v;
     }
+	
+	@Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+	
+//	@Override
+//	public void onInfoWindowClick(Marker paramMarker) {
+//		
+//		
+//		Toast.makeText(mActivity, "Info " + paramMarker.getId() , Toast.LENGTH_LONG).show();
+//	}	
 }
