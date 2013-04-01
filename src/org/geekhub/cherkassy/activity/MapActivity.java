109f2bba54c9585bf9f408e6dd2829fc02953cc3
chebTS <chebTS@gmail.com>
2013-04-02 00:04:35 +0300

    Current location on map added

--- src/org/geekhub/cherkassy/activity/MapActivity.java
+++ src/org/geekhub/cherkassy/activity/MapActivity.java
@@ -1,13 +1,19 @@
 package org.geekhub.cherkassy.activity;
 
-import android.os.Bundle;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 import org.geekhub.cherkassy.R;
 import org.geekhub.cherkassy.fragments.MapFragment;
 
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+
+
 public class MapActivity extends SherlockFragmentActivity {
-	
-	
+	private LocationManager locationManager;
+	private static final long MIN_TIME = 400;
+	private static final float MIN_DISTANCE = 1000;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,31 @@ public class MapActivity extends SherlockFragmentActivity {
             		.add(R.id.map_frag, new MapFragment())
             		.commit();
         }
+		
+	 
 	}
+
+//	@Override
+//	public void onLocationChanged(Location location) {
+//		LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//		fragmentMap.updateLocation(latLng);
+//	    //CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+//	    //map.animateCamera(cameraUpdate);
+//	    locationManager.removeUpdates(this);
+//	}
+//
+//	@Override
+//	public void onProviderDisabled(String provider) {
+//	}
+//
+//	@Override
+//	public void onProviderEnabled(String provider) {
+//	}
+//
+//	@Override
+//	public void onStatusChanged(String provider, int status, Bundle extras) {
+//	}
+	
 
 	
 }
--- src/org/geekhub/cherkassy/fragments/MapFragment.java
+++ src/org/geekhub/cherkassy/fragments/MapFragment.java
@@ -1,21 +1,35 @@
 package org.geekhub.cherkassy.fragments;
 
+import org.geekhub.cherkassy.R;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.actionbarsherlock.app.SherlockFragment;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
-import org.geekhub.cherkassy.R;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MapFragment extends SherlockFragment  {
+public class MapFragment extends SherlockFragment  implements LocationListener{
 	private MapView mMapView;
     private GoogleMap mMap;
     private Bundle mBundle;
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
 	
-	
+
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.map_frag, container,false);
@@ -25,17 +39,49 @@ public class MapFragment extends SherlockFragment  {
 	        mMap = ((MapView) v.findViewById(R.id.map)).getMap();
 	        mMap.setMyLocationEnabled(true);	        
 	        
+	        
+	        try {
+	            MapsInitializer.initialize(getSherlockActivity());
+	        } catch (GooglePlayServicesNotAvailableException e) {
+	            e.printStackTrace();
+	        }
+	        locationManager = (LocationManager) getSherlockActivity().getSystemService(Context.LOCATION_SERVICE);
+	        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this); //You can also use LocationManager.GPS_PROVIDER and LocationManager.PASSIVE_PROVIDER        
+
+
 	        //mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(37.412259, -79.138257) , 13.0f) );
 	        //mMap.setOnInfoWindowClickListener(this);
 	        //addMarkers();
-	        
 		}
         return v;
     }
 	
 	
 	
+	
+	
 
+
+	@Override
+	public void onLocationChanged(Location location) {
+		LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+	    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+	    mMap.animateCamera(cameraUpdate);
+	    locationManager.removeUpdates(this);
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
 
 	@Override
     public void onResume() {
