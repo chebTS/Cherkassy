951f23e14842c9e40de33bc111db2861574fbff2
chebTS <chebTS@gmail.com>
2013-04-09 02:06:46 +0300

    Maps to item added

--- src/org/geekhub/cherkassy/fragments/ItemFragment.java
+++ src/org/geekhub/cherkassy/fragments/ItemFragment.java
@@ -1,5 +1,9 @@
 package org.geekhub.cherkassy.fragments;
 
+import org.geekhub.cherkassy.R;
+import org.geekhub.cherkassy.db.DatabaseHelper;
+import org.geekhub.cherkassy.db.InfoTable;
+
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -7,31 +11,84 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import com.actionbarsherlock.app.SherlockFragment;
-import org.geekhub.cherkassy.R;
-import org.geekhub.cherkassy.db.DatabaseHelper;
-import org.geekhub.cherkassy.db.InfoTable;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class ItemFragment extends SherlockFragment {
-
+	private MapView mMapView;
+    private GoogleMap mMap;
+    private Bundle mBundle;
+	
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.item_frag,container,false);
     }
+    
+    
 
     @Override
     public void onStart() {
         super.onStart();
         long item_id =  getActivity().getIntent().getLongExtra("ID", 0);
-
         DatabaseHelper dbHelper = new DatabaseHelper(getActivity());
         SQLiteDatabase database = dbHelper.getReadableDatabase();
         Cursor cursor = database.query(InfoTable.TABLE_ITEMS, null, InfoTable.COLUMN_ID + "='" + item_id + "'", null,null,null,null);
         cursor.moveToFirst();
         String category =  cursor.getString(cursor.getColumnIndex(InfoTable.COLUMN_NAME));
-
         TextView tw = (TextView) getView().findViewById(R.id.item_title);
         tw.setText(category);
+        if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity()) == 0){
+	        mMapView = (MapView) getView().findViewById(R.id.map);
+	        mMapView.onCreate(mBundle);	
+	        mMap = mMapView.getMap();	
+	        mMap.setMyLocationEnabled(true);
+	        MarkerOptions mo = new MarkerOptions()
+				.position(new LatLng(
+					cursor.getDouble(cursor.getColumnIndex(InfoTable.COLUMN_LATITUDE)),
+					cursor.getDouble(cursor.getColumnIndex(InfoTable.COLUMN_LOGOURL))))
+				.title(cursor.getString(cursor.getColumnIndex(InfoTable.COLUMN_NAME)))
+				.snippet(cursor.getString(cursor.getColumnIndex(InfoTable.COLUMN_ADDRESS))); 
+	        mMap.addMarker(mo);
+//	        Polyline line =mMap.addPolyline(new PolylineOptions().add(
+//	        			new LatLng(56.05, 42.042), 
+//	        			new LatLng(50.00, 45.02))
+//	        			.geodesic(true));
+        }
         
     }
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBundle = savedInstanceState;
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    
 }
