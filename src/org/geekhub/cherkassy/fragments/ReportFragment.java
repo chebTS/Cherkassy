ee1382e609e7e70fe635c125c080387951c4cfb5
chebTS <chebTS@gmail.com>
2013-04-14 23:11:43 +0300

    Alert dialog in report added

--- src/org/geekhub/cherkassy/fragments/ReportFragment.java
+++ src/org/geekhub/cherkassy/fragments/ReportFragment.java
@@ -7,7 +7,10 @@ import org.geekhub.cherkassy.R;
 import org.geekhub.cherkassy.activity.MapActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -43,6 +46,7 @@ public class ReportFragment extends SherlockFragment implements OnClickListener{
 	SherlockFragmentActivity actReport;
 	Uri imageUri;
 	Double lat, lng;
+	AlertDialog.Builder ad;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +58,7 @@ public class ReportFragment extends SherlockFragment implements OnClickListener{
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		btnSend.setOnClickListener(this);
+		imgPhoto.setOnClickListener(this);
 		actReport = getSherlockActivity();
 	}
 
@@ -89,6 +94,25 @@ public class ReportFragment extends SherlockFragment implements OnClickListener{
 				//TODO reset text, imageview and coordinates;
 			}
 			break;
+		case R.id.imgPhoto:
+			ad = new AlertDialog.Builder(getSherlockActivity());
+			ad.setTitle("Choose source");  // заголовок
+			ad.setMessage("Camera or gallery"); // сообщение
+			ad.setCancelable(true);
+			ad.setPositiveButton("Camera", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					openCamera();
+				}
+			});
+			ad.setNeutralButton("Gallery", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					openGallery();
+				}
+			});
+			ad.show();
+			break;
 		default:
 			break;
 		}
@@ -100,31 +124,56 @@ public class ReportFragment extends SherlockFragment implements OnClickListener{
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.gallery:
-				Log.i("Opt","Gallery");
-				Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-				photoPickerIntent.setType("image/*");
-				startActivityForResult(photoPickerIntent, SELECT_GALERY);  
+				openGallery();
+//				Log.i("Opt","Gallery");
+//				Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+//				photoPickerIntent.setType("image/*");
+//				startActivityForResult(photoPickerIntent, SELECT_GALERY);  
 				break;
 			case R.id.camera:
-				Log.i("Opt","Camera");
-				Log.d("ANDRO_CAMERA", "Starting camera on the phone...");
-		        String fileName = "testphoto.jpg";
-		        ContentValues values = new ContentValues();
-		        values.put(MediaStore.Images.Media.TITLE, fileName);
-		        values.put(MediaStore.Images.Media.DESCRIPTION,
-		                "Image capture by camera");
-		        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-		        imageUri = actReport.getContentResolver().insert( MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-		        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-		        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
-		        startActivityForResult(intent, SELECT_CAMERA);
+				openCamera();
+//				Log.i("Opt","Camera");
+//				Log.d("ANDRO_CAMERA", "Starting camera on the phone...");
+//		        String fileName = "testphoto.jpg";
+//		        ContentValues values = new ContentValues();
+//		        values.put(MediaStore.Images.Media.TITLE, fileName);
+//		        values.put(MediaStore.Images.Media.DESCRIPTION,
+//		                "Image capture by camera");
+//		        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+//		        imageUri = actReport.getContentResolver().insert( MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+//		        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//		        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+//		        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+//		        startActivityForResult(intent, SELECT_CAMERA);
 				break;
 			case R.id.map:
 				startActivityForResult(new Intent(getSherlockActivity(), MapActivity.class), SELECT_MAP);
 				break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void openGallery(){
+		Log.i("Opt","Gallery");
+		Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+		photoPickerIntent.setType("image/*");
+		startActivityForResult(photoPickerIntent, SELECT_GALERY); 
+	}
+	
+	private void openCamera(){
+		Log.i("Opt","Camera");
+		Log.d("ANDRO_CAMERA", "Starting camera on the phone...");
+        String fileName = "testphoto.jpg";
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, fileName);
+        values.put(MediaStore.Images.Media.DESCRIPTION,
+                "Image capture by camera");
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        imageUri = actReport.getContentResolver().insert( MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+        startActivityForResult(intent, SELECT_CAMERA);
 	}
 
 	@Override
